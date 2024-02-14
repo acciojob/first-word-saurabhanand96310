7a16f9819b5eb69aa0446eb9ea058e9fa3f6a183
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-14 17:25:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+	let f="";
+	s=s.trim();
+	s=s+" ";
+	for(let t in s ){
+		f=f+t;
+		if(t==" "){
+			break;
+		}
+	}
+	return f;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
